7bef6c3bcb521a097765f7189e612f0d33db0d00
Abhinav Singh <abhinav22singh03@gmail.com>
2023-10-25 20:10:55 +0530

    popup created

--- app/src/main/java/com/example/newsmania/NewsAdapter.java
+++ app/src/main/java/com/example/newsmania/NewsAdapter.java
@@ -1,12 +1,20 @@
 package com.example.newsmania;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,11 +22,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.kwabenaberko.newsapilib.models.Article;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
     private List<Article> newsArticleList;
+    private SavedArticle savedArticle;
     public NewsAdapter(List<Article> newsArticleList) {
         this.newsArticleList = newsArticleList;
 
@@ -35,6 +43,12 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
     public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
         // Bind data from the NewsArticle object to the views in the ViewHolder
         Article newsArticle = newsArticleList.get(position);
+//        savedArticle = new SavedArticle();
+//        savedArticle.setAuthor(newsArticle.getAuthor());
+//        savedArticle.setTitle(newsArticle.getTitle());
+//        savedArticle.setSourceName(newsArticle.getSource().getName());
+//        savedArticle.setUrl(newsArticle.getUrl());
+//        savedArticle.setUrlToImage(newsArticle.getUrlToImage());
         holder.titleTextView.setText(newsArticle.getTitle());
         holder.authorTextView.setText(newsArticle.getAuthor());
         holder.sourceTextView.setText(newsArticle.getSource().getName());
@@ -45,6 +59,43 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
             intent.putExtra("url", newsArticle.getUrl());
             v.getContext().startActivity(intent);
         });
+        holder.popupButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showPopupDialog(v.getContext());
+                Toast.makeText(v.getContext(), "mnggkuyh", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void showPopupDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View popupView = inflater.inflate(R.layout.popup_menu, null);
+
+        // Customize the popup view as needed (e.g., set content, buttons, etc.)
+
+        builder.setView(popupView);
+//        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+//            }
+//        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        ImageButton cross = popupView.findViewById(R.id.cross);
+        cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+
     }
 
     @Override
@@ -69,6 +120,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
         TextView authorTextView;
         TextView sourceTextView;
         ImageView imageView;
+        ImageView popupButton;
 
         public NewsViewHolder(View itemView) {
             super(itemView);
@@ -76,6 +128,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
             authorTextView = itemView.findViewById(R.id.authorTextView);
             sourceTextView = itemView.findViewById(R.id.sourceTextView);
             imageView = itemView.findViewById(R.id.imageView);
+            popupButton = itemView.findViewById(R.id.popupButton);
         }
     }
 
---
+++ app/src/main/java/com/example/newsmania/SavedArticle.java
@@ -0,0 +1,49 @@
+package com.example.newsmania;
+
+public class SavedArticle {
+    private String author;
+    private String title;
+    private String url;
+    private String urlToImage;
+    private String sourceName;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+}
